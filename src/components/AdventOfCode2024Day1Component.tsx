653b37791ef211d88d18655a2cb8d6708b8df0f6
Eric Nguyen <eric@nguyene.com>
2024-12-27 15:33:28 -0500

    refactor: advent of code day 1 code cleanup

--- src/components/AdventOfCode2024Day1Component.tsx
+++ src/components/AdventOfCode2024Day1Component.tsx
@@ -4,30 +4,70 @@ import {
   ColumnLayout,
   Container,
   ContentLayout,
+  ExpandableSection,
   Header,
   InputProps,
   NonCancelableCustomEvent,
   SpaceBetween,
   Textarea,
 } from "@cloudscape-design/components";
-import { AdventOfCode2024Day1Part1ReadInput } from "../utils/AdventOfCode2024Day1Utils";
+import { readWhitespaceDelimitedInput } from
+  "../utils/AdventOfCode2024InputParsingUtils";
 import {
   arraySum,
   getDistances,
   mergeArrays,
 } from "../utils/AdventOfCode2024Utils";
 
+type SimilarityScoreMap = { [key: number]: number };
+
+interface CalculationResults {
+  unsortedLists: number[];
+  sortedLists: number[];
+  distanceList: number[];
+  totalDistance: number;
+  similarityScoresMap: SimilarityScoreMap;
+  similarityScoresList: number[];
+  similarityScore: number;
+}
+
+const calculateResults = (input: string): CalculationResults => {
+  const { list1: unsortedList1, list2: unsortedList2 } =
+    readWhitespaceDelimitedInput(input);
+
+  const unsortedLists = mergeArrays(unsortedList1, unsortedList2);
+  const sortedList1 = unsortedList1.slice().sort();
+  const sortedList2 = unsortedList2.slice().sort();
+  const sortedLists = mergeArrays(sortedList1, sortedList2);
+
+  const distanceList = getDistances(sortedList1, sortedList2);
+  const totalDistance = arraySum(distanceList);
+
+  const unsortedList1Set = new Set(unsortedList1);
+  const similarityScoresMap: SimilarityScoreMap = {};
+  unsortedList1Set.forEach((n) => {
+    similarityScoresMap[n] = unsortedList2.filter((n2) => n2 === n).length;
+  });
+
+  const similarityScoresList = unsortedList1.map(
+    (n) => n * (similarityScoresMap[n] || 0)
+  );
+  const similarityScore = arraySum(similarityScoresList);
+
+  return {
+    unsortedLists,
+    sortedLists,
+    distanceList,
+    totalDistance,
+    similarityScoresMap,
+    similarityScoresList,
+    similarityScore
+  };
+}
+
 export const AdventOfCode2024Day1Component = (): ReactElement => {
   const [rawInput, setRawInput] = useState("");
-  const [unsortedLists, setUnsortedLists] = useState<number[]>([]);
-  const [sortedLists, setSortedLists] = useState<number[]>([]);
-  const [distanceList, setDistanceList] = useState<number[]>([]);
-  const [totalDistance, setTotalDistance] = useState(0);
-  const [similarityScoresMap, setSimilarityScoresMap] = useState<number[]>([]);
-  const [similarityScoresList, setSimilarityScoresList] = useState<number[]>(
-    []
-  );
-  const [similarityScore, setSimilarityScore] = useState(0);
+  const [results, setResults] = useState<CalculationResults | null>(null);
 
   const handleInput = (
     event: NonCancelableCustomEvent<InputProps.ChangeDetail>
@@ -35,50 +75,7 @@ export const AdventOfCode2024Day1Component = (): ReactElement => {
     const input = event.detail.value;
 
     setRawInput(input);
-
-    const { list1: unsortedList1, list2: unsortedList2 } =
-      AdventOfCode2024Day1Part1ReadInput(input);
-
-    setUnsortedLists(mergeArrays(unsortedList1, unsortedList2));
-
-    const sortedList1 = unsortedList1.sort();
-    const sortedList2 = unsortedList2.sort();
-
-    setSortedLists(mergeArrays(sortedList1, sortedList2));
-
-    const distances = getDistances(sortedList1, sortedList2);
-
-    setDistanceList(distances);
-
-    const totalDistance = arraySum(distances);
-
-    setTotalDistance(totalDistance);
-
-    const unsortedList1Set = new Set(unsortedList1);
-    const similarityScoreMap: { [n: number]: number } = {};
-
-    unsortedList1Set.forEach((n) => {
-      similarityScoreMap[n] = unsortedList2.filter((n2) => n2 === n).length;
-    });
-
-    const similarityScoreNumberSet = Object.keys(similarityScoreMap).map((n) =>
-      parseInt(n, 10)
-    );
-    const similarityScoreNumberSetMapping = Object.values(similarityScoreMap);
-
-    setSimilarityScoresMap(
-      mergeArrays(similarityScoreNumberSet, similarityScoreNumberSetMapping)
-    );
-
-    const similarityScoreList = unsortedList1.map((n) => {
-      return n * similarityScoreMap[n];
-    });
-
-    setSimilarityScoresList(similarityScoreList);
-
-    const similarityScore = arraySum(similarityScoreList);
-
-    setSimilarityScore(similarityScore);
+    setResults(calculateResults(input));
   };
 
   return (
@@ -92,22 +89,77 @@ export const AdventOfCode2024Day1Component = (): ReactElement => {
         <Container>
           <SpaceBetween direction="vertical" size="xs">
             <Header variant="h2">Day 1</Header>
-            <Header variant="h3">Raw Input</Header>
+            <p>
+              Given a text input of two whitespace-delimited columns of numbers,
+              sort each list and get the sum of the distance between each
+              number, shown as the <strong>Total Distance</strong>.
+            </p>
+
+            <p>
+              Next, for each number in the first column, count the occurrences
+              in which it appears in the second column, and multiply the number
+              by the occurrence count. The sum of the products
+              of each number and its occurrence count is shown as the{" "}
+              <strong>similarity score</strong>.
+            </p>
+
             <Textarea value={rawInput} onChange={handleInput} />
-            <Header variant="h3">Unsorted Lists</Header>
-            <ColumnLayout columns={2}>{unsortedLists}</ColumnLayout>
-            <Header variant="h3">Sorted Lists</Header>
-            <ColumnLayout columns={2}>{sortedLists}</ColumnLayout>
-            <Header variant="h3">Distance List</Header>
-            <ColumnLayout columns={1}>{distanceList}</ColumnLayout>
-            <Header variant="h3">Total distance between two lists</Header>
-            <p>{totalDistance}</p>
-            <Header variant="h3">Similarity Scores Mapping</Header>
-            <ColumnLayout columns={2}>{similarityScoresMap}</ColumnLayout>
-            <Header variant="h3">Similarity Scores List</Header>
-            <ColumnLayout columns={1}>{similarityScoresList}</ColumnLayout>
-            <Header variant="h3">Similarity Score</Header>
-            <p>{similarityScore}</p>
+
+            {results && (
+              <>
+                <ExpandableSection headerText="Unsorted Lists">
+                  <ColumnLayout columns={2}>
+                    {results.unsortedLists.map((n, i) => (
+                      <p key={`unsorted-${i}`}>{n}</p>
+                    ))}
+                  </ColumnLayout>
+                </ExpandableSection>
+
+                <ExpandableSection headerText="Sorted Lists">
+                  <ColumnLayout columns={2}>
+                    {results.sortedLists.map((n, i) => (
+                      <p key={`sorted-${i}`}>{n}</p>
+                    ))}
+                  </ColumnLayout>
+                </ExpandableSection>
+
+                <ExpandableSection headerText="Distance List">
+                  <ColumnLayout columns={1}>
+                    {results.distanceList.map((n, i) => (
+                      <p key={`distance-${i}`}>{n}</p>
+                    ))}
+                  </ColumnLayout>
+                </ExpandableSection>
+
+                <ExpandableSection headerText="Total Distance">
+                  <p>{results.totalDistance}</p>
+                </ExpandableSection>
+
+                <ExpandableSection headerText="Similarity Scores Mapping">
+                  <ColumnLayout columns={1}>
+                    {Object.entries(results.similarityScoresMap).map(
+                      ([key, value], i) => (
+                        <p key={`scoremap-${i}`}>
+                          {key}: {value}
+                        </p>
+                      )
+                    )}
+                  </ColumnLayout>
+                </ExpandableSection>
+
+                <ExpandableSection headerText="Similarity Scores List">
+                  <ColumnLayout columns={1}>
+                    {results.similarityScoresList.map((n, i) => (
+                      <p key={`scorelist-${i}`}>{n}</p>
+                    ))}
+                  </ColumnLayout>
+                </ExpandableSection>
+
+                <ExpandableSection headerText="Similarity Score">
+                  <p>{results.similarityScore}</p>
+                </ExpandableSection>
+              </>
+            )}
           </SpaceBetween>
         </Container>
       </ContentLayout>
--- src/utils/AdventOfCode2024InputParsingUtils.ts
+++ src/utils/AdventOfCode2024InputParsingUtils.ts
@@ -1,11 +1,17 @@
-export const AdventOfCode2024Day1Part1ReadInput = (
+/**
+ * Input parsing utility for two lists of numbers.
+ * 
+ * @param input The string input.
+ * @returns The two list of numbers as a map.
+ */
+export const readWhitespaceDelimitedInput = (
   input: string
 ): { list1: number[]; list2: number[] } => {
   const result: { list1: number[]; list2: number[] } = { list1: [], list2: [] };
 
   const inputArray = input.split("\n");
   inputArray.forEach((item) => {
-    const [n1, n2] = item.split("   ");
+    const [n1, n2] = item.split(/s+/);
 
     result.list1.push(parseInt(n1, 10));
     result.list2.push(parseInt(n2, 10));
